c7f9781f767f7e812d2e64d6c29c36d013a630d1
Lukas Andreas Muller <lamuller@protonmail.com>
2016-12-03 13:41:45 -0800

    Change some things

--- hello-persistence/Sources/App/main.swift
+++ hello-persistence/Sources/App/main.swift
@@ -15,6 +15,31 @@ drop.get("test") { request in
   return try JSON(node: Acronym.all().makeNode())
 }
 
+drop.get("all") { request in
+  return try JSON(node: Acronym.all().makeNode())
+}
+
+drop.get("first") { request in
+  return try JSON(node: Acronym.all().first?.makeNode())
+}
+
+drop.get("ily") { request in
+  return try JSON(node: Acronym.query().filter("short", "Ily").all().makeNode())
+}
+
+drop.get("notily") { request in
+  return try JSON(node: Acronym.query().filter("short", .notEquals, "Ily").all().makeNode())
+}
+
+drop.get("update") { request in
+  guard var first = try Acronym.query().first(),
+    let long = request.data["long"]?.string else {
+      throw Abort.badRequest
+  }
+  first.long = long
+  try first.save()
+  return first
+}
 
 
 drop.run()
